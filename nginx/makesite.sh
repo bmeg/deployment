1b7f56f762cae7331c554a6ae29f5eba2bebcae2
Brian Walsh <brian@bwalsh.com>
2019-03-06 13:35:31 -0800

    removes sudo

--- nginx/makesite.sh
+++ nginx/makesite.sh
@@ -1,16 +1,16 @@
-source  ../nginx.env ; export $(cut -d= -f1 ../nginx.env) ; 
+source  ../nginx.env ; export $(cut -d= -f1 ../nginx.env) ;
 cd site-builder
 docker build -t site-builder .
 cd ..
 echo Cloning static site ..................
-sudo rm -rf bmeg-site || true
+rm -rf bmeg-site || true
 git clone https://github.com/bmeg/bmeg-site
-chmod -R +rw bmeg-site 
-cd bmeg-site ; 
-  git fetch --all ; git checkout $BMEG_SITE_BRANCH  
-  echo Setting base url in hugo config ..................  
-  sed -i.bak 's~baseURL:.*~baseURL: '$BASE_URL'/~' config.yaml  
-  docker run --rm -v $PWD:/bmeg/bmeg-site site-builder make build  
-  echo copying static site to nginx ..................  
-  rm -rf ../docker/etc-nginx/public || true  
+chmod -R +rw bmeg-site
+cd bmeg-site ;
+  git fetch --all ; git checkout $BMEG_SITE_BRANCH
+  echo Setting base url in hugo config ..................
+  sed -i.bak 's~baseURL:.*~baseURL: '$BASE_URL'/~' config.yaml
+  docker run --rm -v $PWD:/bmeg/bmeg-site site-builder make build
+  echo copying static site to nginx ..................
+  rm -rf ../docker/etc-nginx/public || true
   cp -r public ../docker/etc-nginx/public
