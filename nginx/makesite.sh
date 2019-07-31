ae3fa238846c7a576fa6818bd92ecd70908be6ec
Brian Walsh <brian@bwalsh.com>
2019-07-31 16:55:20 +0000

    adds bmeg.io

--- nginx/makesite.sh
+++ nginx/makesite.sh
@@ -6,11 +6,10 @@ echo Cloning static site ..................
 rm -rf bmeg-site || true
 git clone https://github.com/bmeg/bmeg-site
 chmod -R +rw bmeg-site
+chgrp -R sudo bmeg-site
 cd bmeg-site ;
-  git fetch --all ; git checkout $BMEG_SITE_BRANCH
+  git fetch --all ; git checkout $BMEG_SITE_BRANCH ; git pull origin $BMEG_SITE_BRANCH
   echo Setting base url in hugo config ..................
   sed -i.bak 's~baseURL:.*~baseURL: '$BASE_URL'/~' config.yaml
   docker run --rm -v $PWD:/bmeg/bmeg-site site-builder make build
-  echo copying static site to nginx ..................
-  rm -rf ../docker/etc-nginx/public || true
-  cp -r public ../docker/etc-nginx/public
+  echo done
