a946423696e4e6dd75d24aaafe281a302274a798
Brian Walsh <brian@bwalsh.com>
2019-09-11 18:56:34 +0000

    Adds multiple sites and comments

--- nginx/makesite.sh
+++ nginx/makesite.sh
@@ -1,13 +1,17 @@
-source  ../nginx.env ; export $(cut -d= -f1 ../nginx.env) ;
+# get the configuration and export it
+source  ../secrets/nginx.env ; export $(cut -d= -f1 ../secrets/nginx.env) ;
+echo Building bmeg site branch=$BMEG_SITE_BRANCH for server $SERVER_NAME
+# ensure the docker image that does the build is up to date
 cd site-builder
 docker build -t site-builder .
 cd ..
+#build it
 echo Cloning static site ..................
-rm -rf bmeg-site || true
-git clone https://github.com/bmeg/bmeg-site
-chmod -R +rw bmeg-site
-chgrp -R sudo bmeg-site
-cd bmeg-site ;
+rm -rf bmeg-site/$SERVER_NAME || true
+git clone https://github.com/bmeg/bmeg-site bmeg-site/$SERVER_NAME
+chmod -R +rw bmeg-site/$SERVER_NAME
+chgrp -R sudo bmeg-site/$SERVER_NAME
+cd bmeg-site/$SERVER_NAME ;
   git fetch --all ; git checkout $BMEG_SITE_BRANCH ; git pull origin $BMEG_SITE_BRANCH
   echo Setting base url in hugo config ..................
   sed -i.bak 's~baseURL:.*~baseURL: '$BASE_URL'/~' config.yaml
